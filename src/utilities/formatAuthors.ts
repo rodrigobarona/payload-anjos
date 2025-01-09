dce59a33a5d3698b48fb4a1637584edefa8ccc24
Adrian Maj <adrianmaj1122@gmail.com>
2025-01-09 07:46:52 +0100

    chore: utilities cleanup

--- src/utilities/formatAuthors.ts
+++ src/utilities/formatAuthors.ts
@@ -1,4 +1,4 @@
-import { Post } from '@/payload-types'
+import { Post } from "@/payload-types";
 
 /**
  * Formats an array of populatedAuthors from Posts into a prettified string.
@@ -10,19 +10,16 @@ import { Post } from '@/payload-types'
  * [Author1, Author2, Author3] becomes 'Author1, Author2, and Author3'
  *
  */
-export const formatAuthors = (
-  authors: NonNullable<NonNullable<Post['populatedAuthors']>[number]>[],
-) => {
+export const formatAuthors = (authors: NonNullable<NonNullable<Post["populatedAuthors"]>[number]>[]) => {
   // Ensure we don't have any authors without a name
-  const filteredAuthors = authors.filter((author) => Boolean(author.name))
+  const filteredAuthors = authors.filter((author) => Boolean(author.name));
 
-  if (filteredAuthors.length === 0) return ''
-  if (filteredAuthors.length === 1) return filteredAuthors[0].name
-  if (filteredAuthors.length === 2)
-    return `${filteredAuthors[0].name} and ${filteredAuthors[1].name}`
+  if (filteredAuthors.length === 0) return "";
+  if (filteredAuthors.length === 1) return filteredAuthors[0].name;
+  if (filteredAuthors.length === 2) return `${filteredAuthors[0].name} and ${filteredAuthors[1].name}`;
 
   return `${filteredAuthors
     .slice(0, -1)
     .map((author) => author?.name)
-    .join(', ')} and ${filteredAuthors[authors.length - 1].name}`
-}
+    .join(", ")} and ${filteredAuthors[authors.length - 1].name}`;
+};
--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -1,39 +1,37 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-import type { Media, Page, Post, Config } from '../payload-types'
+import type { Media, Page, Post, Config } from "@/payload-types";
 
-import { mergeOpenGraph } from './mergeOpenGraph'
-import { getServerSideURL } from './getURL'
+import { mergeOpenGraph } from "./mergeOpenGraph";
+import { getServerSideURL } from "./getURL";
 
-const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
-  const serverUrl = getServerSideURL()
+const getImageURL = (image?: Media | Config["db"]["defaultIDType"] | null) => {
+  const serverUrl = getServerSideURL();
 
-  let url = serverUrl + '/website-template-OG.webp'
+  let url = serverUrl + "/website-template-OG.webp";
 
-  if (image && typeof image === 'object' && 'url' in image) {
-    const ogUrl = image.sizes?.og?.url
+  if (image && typeof image === "object" && "url" in image) {
+    const ogUrl = image.sizes?.og?.url;
 
-    url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url
+    url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url;
   }
 
-  return url
-}
+  return url;
+};
 
-export const generateMeta = async (args: {
-  doc: Partial<Page> | Partial<Post>
-}): Promise<Metadata> => {
-  const { doc } = args || {}
+export const generateMeta = async (args: { doc: Partial<Page> | Partial<Post> }): Promise<Metadata> => {
+  const { doc } = args || {};
 
-  const ogImage = getImageURL(doc?.meta?.image)
+  const ogImage = getImageURL(doc?.meta?.image);
 
   const title = doc?.meta?.title
-    ? doc?.meta?.title + ' | Payload Website Template'
-    : 'Payload Website Template'
+    ? doc?.meta?.title + " | Payload Website Template"
+    : "Payload Website Template";
 
   return {
     description: doc?.meta?.description,
     openGraph: mergeOpenGraph({
-      description: doc?.meta?.description || '',
+      description: doc?.meta?.description || "",
       images: ogImage
         ? [
             {
@@ -42,8 +40,8 @@ export const generateMeta = async (args: {
           ]
         : undefined,
       title,
-      url: Array.isArray(doc?.slug) ? doc?.slug.join('/') : '/',
+      url: Array.isArray(doc?.slug) ? doc?.slug.join("/") : "/",
     }),
     title,
-  }
-}
+  };
+};
