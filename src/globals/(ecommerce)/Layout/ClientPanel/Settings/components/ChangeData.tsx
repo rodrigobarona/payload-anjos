3c94268b7915632fe2e1ff94890f87a43ec21369
Adrian Maj <adrianmaj1122@gmail.com>
2025-01-28 12:53:46 +0100

    feat: data changing in user settings

---
+++ src/globals/(ecommerce)/Layout/ClientPanel/Settings/components/ChangeData.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormControl, FormMessage } from "@/components/ui/form";
+import { Input } from "@headlessui/react";
+import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const ChangeData = ({
+  value,
+  text,
+  altText,
+  name,
+  userID,
+  type = "text",
+}: {
+  value: string;
+  text: string;
+  altText: string;
+  name: string;
+  userID: string;
+  type?: string;
+}) => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const onSubmit = async (values) => {
+    // console.log(values);
+    try {
+      await axios.patch(
+        `/api/customers/${userID}`,
+        { [name]: type === "date" ? new Date(values[name]).toISOString() : values[name] },
+        {
+          withCredentials: true,
+        },
+      );
+    } catch (error) {
+      console.error(error);
+      form.reset();
+    }
+  };
+  const form = useForm({
+    defaultValues: {
+      [name]: type === "date" ? value.split("T")[0] : value,
+    },
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full">
+        <FormField
+          control={form.control}
+          name={name}
+          render={({ field }) => (
+            <FormItem className="flex w-full items-center">
+              <FormControl>
+                <Input
+                  className="w-full border-none p-0 text-gray-900 outline-none ring-0 focus-within:outline-none focus:outline-none"
+                  type={type}
+                  readOnly={!isEnabled}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          variant="link"
+          type={isEnabled ? "button" : "submit"}
+          onClick={() => setIsEnabled(!isEnabled)}
+          className="items-center py-2 font-semibold text-indigo-600 hover:text-indigo-500"
+        >
+          {isEnabled ? altText : text}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+export default ChangeData;
--- src/globals/(ecommerce)/Layout/ClientPanel/Settings/index.tsx
+++ src/globals/(ecommerce)/Layout/ClientPanel/Settings/index.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { Customer } from "@/payload-types";
-import { formatDateTime } from "@/utilities/formatDateTime";
 import { useTranslations } from "next-intl";
 import { ChangePassword } from "./components/ChangePassword";
+import ChangeData from "./components/ChangeData";
 
 export const Settings = ({ user }: { user: Customer }) => {
   const t = useTranslations("Account.settings");
@@ -13,19 +12,30 @@ export const Settings = ({ user }: { user: Customer }) => {
       <dl className="mt-6 divide-y divide-gray-100 border-gray-200 text-sm/6">
         <div className="items-center py-6 sm:flex">
           <dt className="h-fit items-center font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-            {t("name")}
+            {t("firstName")}
           </dt>
           <dd className="mt-1 flex items-center justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-            <div className="text-gray-900">
-              {user.firstName} {user.lastName}
-            </div>
-            <Button
-              variant="link"
-              type="button"
-              className="items-center py-2 font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              {t("update")}
-            </Button>
+            <ChangeData
+              value={user.firstName ?? ""}
+              text={t("update")}
+              altText={t("save")}
+              name="firstName"
+              userID={user.id}
+            />
+          </dd>
+        </div>
+        <div className="items-center py-6 sm:flex">
+          <dt className="h-fit items-center font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+            {t("lastName")}
+          </dt>
+          <dd className="mt-1 flex items-center justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+            <ChangeData
+              value={user.lastName ?? ""}
+              text={t("update")}
+              altText={t("save")}
+              name="lastName"
+              userID={user.id}
+            />
           </dd>
         </div>
         <div className="items-center py-6 sm:flex">
@@ -33,14 +43,13 @@ export const Settings = ({ user }: { user: Customer }) => {
             {t("email")}
           </dt>
           <dd className="mt-1 flex items-center justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-            <div className="text-gray-900">{user.email}</div>
-            <Button
-              variant="link"
-              type="button"
-              className="items-center py-2 font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              {t("update")}
-            </Button>
+            <ChangeData
+              value={user.email}
+              text={t("update")}
+              altText={t("save")}
+              name="email"
+              userID={user.id}
+            />
           </dd>
         </div>
         <div className="items-center py-6 sm:flex">
@@ -57,14 +66,14 @@ export const Settings = ({ user }: { user: Customer }) => {
             {t("birthDate")}
           </dt>
           <dd className="mt-1 flex items-center justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-            <div className="text-gray-900">{user.birthDate && formatDateTime(user.birthDate, "EU")}</div>
-            <Button
-              variant="link"
-              type="button"
-              className="items-center py-2 font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              {t("update")}
-            </Button>
+            <ChangeData
+              value={user.birthDate ?? ""}
+              text={t("update")}
+              type="date"
+              altText={t("save")}
+              name="birthDate"
+              userID={user.id}
+            />
           </dd>
         </div>
       </dl>
