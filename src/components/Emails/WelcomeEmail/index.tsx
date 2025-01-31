c96af51cc6aeebfe159ab4daa1187d965836f1f3
Adrian Maj <adrianmaj1122@gmail.com>
2025-01-31 08:18:42 +0100

    feat: welcome email

--- src/components/Emails/WelcomeEmail/index.tsx
+++ src/components/Emails/WelcomeEmail/index.tsx
@@ -2,19 +2,19 @@ import { Html } from "@react-email/components";
 import { type ReactNode } from "react";
 
 import { type Locale } from "@/i18n/config";
-import { type Order } from "@/payload-types";
+import { type Customer } from "@/payload-types";
 import { getCachedGlobal } from "@/utilities/getGlobals";
 
 import { Default } from "./variants/Default";
 
-export const OrderStatusEmail = async ({ order, locale }: { order: Order; locale: Locale }) => {
+export const WelcomeEmail = async ({ customer, locale }: { customer: Customer; locale: Locale }) => {
   const { messages } = await getCachedGlobal("emailMessages", locale, 1)();
 
   let Email: ReactNode = <Html></Html>;
 
   switch (messages.template) {
     case "default":
-      Email = <Default order={order} locale={locale} />;
+      Email = <Default customer={customer} locale={locale} />;
   }
   return Email;
 };
