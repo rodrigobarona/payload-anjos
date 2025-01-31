6f5f2c4f20e8e261c40535daef0c7e906aef52df
Adrian Maj <adrianmaj1122@gmail.com>
2025-01-31 15:50:10 +0100

    fix: testing login loop fix

--- src/app/(frontend)/[locale]/(with-cart)/account/layout.tsx
+++ src/app/(frontend)/[locale]/(with-cart)/account/layout.tsx
@@ -1,10 +1,13 @@
+import { headers } from "next/headers";
 import { setRequestLocale } from "next-intl/server";
+import { getPayload } from "payload";
 import { type ReactNode } from "react";
 
 import { ClientPanel } from "@/globals/(ecommerce)/Layout/ClientPanel/Component";
 import { type Locale } from "@/i18n/config";
 import { redirect } from "@/i18n/routing";
-import { getCustomer } from "@/utilities/getCustomer";
+// import { getCustomer } from "@/utilities/getCustomer";
+import config from "@payload-config";
 
 export const dynamic = "force-dynamic";
 
@@ -15,9 +18,13 @@ const AccountPage = async ({
   params: Promise<{ locale: Locale }>;
   children: ReactNode;
 }) => {
-  const user = await getCustomer();
+  // const user = await getCustomer();
   const { locale } = await params;
   setRequestLocale(locale);
+  const authHeaders = await headers();
+  const payload = await getPayload({ config });
+
+  const { user } = await payload.auth({ headers: authHeaders });
 
   if (!user) {
     return redirect({ locale, href: "/login" });
