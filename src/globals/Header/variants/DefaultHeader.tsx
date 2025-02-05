3ec6ab06b58c46c5b1336a54b6cea455215fa0ae
Adrian Maj <adrianmaj1122@gmail.com>
2025-02-05 08:46:29 +0100

    fix: logo fix

--- src/globals/Header/variants/DefaultHeader.tsx
+++ src/globals/Header/variants/DefaultHeader.tsx
@@ -56,6 +56,7 @@ export const DefaultHeader = ({ data, theme }: { data: Header; theme: string | n
           {data.logo && typeof data.logo !== "string" && data.logo.url && data.logo.alt ? (
             <Media
               resource={data.logo}
+              imgClassName="h-[88px] w-full max-w-[9.375rem]"
               className={`${isMenuOpened && "invert lg:invert-0"} -my-7 h-[88px] w-full max-w-[9.375rem]`}
             />
           ) : (
--- src/globals/Header/variants/FloatingHeader.tsx
+++ src/globals/Header/variants/FloatingHeader.tsx
@@ -62,6 +62,7 @@ export const FloatingHeader = ({ data, theme }: { data: Header; theme: string | 
           {data.logo && typeof data.logo !== "string" && data.logo.url && data.logo.alt ? (
             <Media
               resource={data.logo}
+              imgClassName="h-[88px] w-full max-w-[9.375rem]"
               className={`${isMenuOpened && "invert lg:invert-0"} -my-7 h-[88px] w-full max-w-[9.375rem]`}
             />
           ) : (
