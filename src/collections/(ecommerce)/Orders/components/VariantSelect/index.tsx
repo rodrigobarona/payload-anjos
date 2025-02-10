0977f74ab097c619a375329e2fcddf7f578cc292
Adrian Maj <adrianmaj1122@gmail.com>
2025-02-10 09:07:16 +0100

    fix: no need to save to choose variant now

--- src/collections/(ecommerce)/Orders/components/VariantSelect/index.tsx
+++ src/collections/(ecommerce)/Orders/components/VariantSelect/index.tsx
@@ -1,7 +1,5 @@
-import { FieldLabel, Select } from "@payloadcms/ui";
+import { FieldLabel } from "@payloadcms/ui";
 import { type TextFieldServerComponent } from "payload";
-
-import { type Product } from "@/payload-types";
 
 import { VariantSelectClient } from "./VariantSelect.client";
 
@@ -10,47 +8,11 @@ export type VariantsArr = {
   value: string | null | undefined;
 }[];
 
-export const VariantSelect: TextFieldServerComponent = async ({ siblingData, req, path }) => {
-  const productID = siblingData.product as string;
-  console.log(productID);
-  const payload = req.payload;
-  let product: Product | null = null;
-  try {
-    product = await payload.findByID({
-      collection: "products",
-      id: productID,
-    });
-    if (!product?.variants) {
-      throw new Error("No variants found");
-    }
-  } catch {
-    return (
-      <div className="max-w-1/2 twp mx-[5px] my-auto h-fit w-full flex-1">
-        <FieldLabel label="Wariant" />
-        <Select
-          value={{
-            label: "Brak",
-            value: "nd",
-          }}
-          disabled
-          options={[]}
-        />
-        <p className="mt-2 text-sm" style={{ color: "var(--theme-eleveation-150)" }}>
-          Jeśli dodajesz produkt ręcznie, to warianty pojawią się po zapisaniu produktu
-        </p>
-      </div>
-    );
-  }
-
-  const variantsArr: VariantsArr = product.variants.map((variant) => ({
-    label: variant.variantSlug,
-    value: variant.variantSlug,
-  }));
-
+export const VariantSelect: TextFieldServerComponent = async ({ path }) => {
   return (
     <div className="max-w-1/2 twp mx-[5px] my-auto h-fit w-full flex-1">
       <FieldLabel label="Wariant" />
-      <VariantSelectClient variantsArr={variantsArr} path={path} />
+      <VariantSelectClient path={path} />
     </div>
   );
 };
