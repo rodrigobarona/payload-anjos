608d7d08a363e9decc1483f5e1e8fd22f133efa4
Adrian Maj <adrianmaj1122@gmail.com>
2025-02-21 09:47:47 +0100

    chore: payload + nextjs bump

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,19 +1,18 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-
-import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
-
-import config from '@payload-config'
-
-import { importMap } from '../importMap'
-
 import type { Metadata } from 'next'
 
-interface Args {
+import config from '@payload-config'
+import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+import { importMap } from '../importMap'
+
+type Args = {
   params: Promise<{
     segments: string[]
   }>
-  searchParams: Promise<Record<string, string | string[]>>
+  searchParams: Promise<{
+    [key: string]: string | string[]
+  }>
 }
 
 export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
